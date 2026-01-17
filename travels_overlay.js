504c97b4ebc46e26cb99e0415a960fc132161494
emilygrocott <emilyjgrocott@gmail.com>
2026-01-16 19:00:50 -0500

    no scroll added for overlay

--- travels_overlay.js
+++ travels_overlay.js
@@ -1,7 +1,9 @@
 function openOverlay() {
   document.getElementById("overlay").style.display = "flex";
+  document.body.classList.add('no-scroll')
 }
 
 function closeOverlay() {
   document.getElementById("overlay").style.display = "none";
+  document.body.classList.remove('no-scroll')
 }
